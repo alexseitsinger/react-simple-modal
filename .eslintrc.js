db5a6beae1e667d69076e4a45db96fe4be387838
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-11-29 20:24:27 -0500

    chore: Adds globals.

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,13 +2,28 @@ const path = require("path")
 
 module.exports = {
   root: true,
+  env: {
+    node: true,
+    browser: true,
+    jest: true,
+  },
+  globals: {
+    window: true,
+    document: true,
+    describe: true,
+    it: true,
+    test: true,
+    mount: true,
+    render: true,
+    shallow: true,
+  },
   settings: {
     "import/ignored": [
       "node_modules",
     ],
     "import/resolver": {
       webpack: {
-        config: path.resolve("./webpack.config.js")
+        config: path.resolve("./webpack.config.dev.js")
       },
     },
   },
