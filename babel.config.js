c90cc16faea9f4f80631d43fcaef276b85b60c5f
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-12-13 14:11:11 -0500

    chore: Adds preset & plugins.

--- babel.config.js
+++ babel.config.js
@@ -4,15 +4,25 @@ module.exports = {
       "useBuiltIns": "entry",
       "corejs": 3
     }],
-    "@babel/preset-react"
+    "@babel/preset-react",
+    ["@emotion/babel-preset-css-prop", {
+      autoLabel: (process.env.NODE_ENV !== "production"),
+      labelFormat: "[local]"
+    }],
   ],
   plugins: [
-    "babel-plugin-emotion",
     "@babel/plugin-transform-runtime",
     "@babel/plugin-proposal-class-properties",
     "@babel/plugin-proposal-export-default-from",
     "@babel/plugin-proposal-export-namespace-from",
     "@babel/plugin-syntax-export-default-from",
     "@babel/plugin-syntax-export-namespace-from"
-  ]
+  ],
+  env: {
+    production: {
+      plugins: [
+        "babel-plugin-transform-react-remove-prop-types",
+      ],
+    },
+  },
 }
