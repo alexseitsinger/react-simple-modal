c4e7d890fb67eeb24915ce88b0ed4ce0066bb3f5
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2020-02-22 09:20:13 -0500

    chore: Splits up javascript config.

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,43 +3,14 @@ const path = require("path")
 const typescriptConfig = require("./.eslintrc-typescript")
 const packageConfig = require("./.eslintrc-package")
 const markdownConfig = require("./.eslintrc-markdown")
+const javascriptConfig = require("./.eslintrc-javascript")
 
 module.exports = {
-  parserOptions: {
-    ecmaVersion: 9,
-    sourceType: "module",
-  },
-  settings: {
-    "import/resolver": {
-      jest: {
-        jestConfigFile: "./jest.config.js",
-      },
-      webpack: {
-        config: path.resolve("./webpack.config.dev.js"),
-      },
-    },
-  },
-  plugins: [
-    "node",
-    "import",
-    "simple-import-sort",
-    "react",
-    "jest",
-    "jest-formatting",
-  ],
-  extends: [
-    "@alexseitsinger/eslint-config/eslint",
-    "@alexseitsinger/eslint-config/node",
-    "@alexseitsinger/eslint-config/import",
-    "@alexseitsinger/eslint-config/simple-import-sort",
-    "@alexseitsinger/eslint-config/react",
-    "@alexseitsinger/eslint-config/jest",
-    "@alexseitsinger/eslint-config/jest-formatting",
-    "prettier",
-    "prettier/babel",
-    "prettier/react",
-  ],
   overrides: [
+    {
+      files: ["*.js", "*.jsx"],
+      ...javascriptConfig,
+    },
     {
       files: ["*.ts", "*.tsx"],
       ...typescriptConfig,
